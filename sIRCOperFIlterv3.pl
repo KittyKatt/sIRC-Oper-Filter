bb6f3e0d8b5626182f3a6f6d7b0802d73bc3cf2b
r00t <r00t@spotchat.org>
2016-03-12 22:43:10 -0500

    Removed superfluous colon and colorcodes

--- sIRCOperFIlterv3.pl
+++ sIRCOperFIlterv3.pl
@@ -44,87 +44,87 @@ sub filter_snotice {
 	my $servername = HexChat::get_info("server");
 	if ($line =~ m/REMOTECONNECT/) {
 		$line =~ s/^\*\*\* REMOTECONNECT: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Connect (R)\cC30]\cC18:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Connect (R)\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/CONNECT/) {
 		$line =~ s/^\*\*\* CONNECT: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Connect\cC30]\cC18:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Connect\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTEQUIT/) {
 		$line =~ s/^\*\*\* REMOTEQUIT: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC29Disconnect (R)\cC30]\cC29:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC29Disconnect (R)\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/QUIT/) {
 		$line =~ s/^\*\*\* QUIT: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC29Disconnect\cC30]\cC29:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC29Disconnect\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTEKILL/) {
 		$line =~ s/^\*\*\* REMOTEKILL: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Kill (R)\cC30]\cC18:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Kill (R)\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/KILL/) {
 		$line =~ s/^\*\*\* KILL: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Kill\cC30]\cC18:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC18Kill\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/DEBUG/) {
 		$line =~ s/^\*\*\* DEBUG: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Debug\cC30]\cC20:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Debug\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTELINK/) {
 		$line =~ s/^\*\*\* REMOTELINK: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Link (R)\cC30]\cC19:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Link (R)\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/LINK/) {
 		$line =~ s/^\*\*\* LINK: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Link\cC30]\cC19:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Link\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTEOPER/) {
 		$line =~ s/^\*\*\* REMOTEOPER: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Oper (R)\cC30]\cC19:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Oper (R)\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/OPER/) {
 		$line =~ s/^\*\*\* OPER: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Oper\cC30]\cC19:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC19Oper\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTEANNOUNCEMENT/) {
 		$line =~ s/^\*\*\* REMOTEANNOUNCEMENT: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Announce (R)\cC30]\cC20:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Announce (R)", $line);
 		return 1;
 	} elsif ($line =~ m/ANNOUNCEMENT/) {
 		$line =~ s/^\*\*\* ANNOUNCEMENT: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Announce\cC30]\cC20:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Announce\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/STATS/) {
 		$line =~ s/^\*\*\* STATS: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Stats\cC30]\cC20:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20Stats\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTEGLOBOPS/) {
 		$line =~ s/^\*\*\* REMOTEGLOBOPS: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC23Global OPs\cC30]\cC23:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC23Global OPs\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/GLOBOPS/) {
 		$line =~ s/^\*\*\* GLOBOPS: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC23Global OPs\cC30]\cC23:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC23Global OPs\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTECHANCREATE/) {
 		$line =~ s/^\*\*\* REMOTECHANCREATE: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Channel Creation\cC30]\cC27:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Channel Creation\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/CHANCREATE/) {
 		$line =~ s/^\*\*\* CHANCREATE: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Channel Creation\cC30]\cC27:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Channel Creation\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/REMOTENICK/) {
 		$line =~ s/^\*\*\* REMOTENICK: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Nick Change\cC30]\cC27:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Nick Change\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/NICK/) {
 		$line =~ s/^\*\*\* NICK: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Nick Change\cC30]\cC27:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC27Nick Change\cC30]", $line);
 		return 1;
 	} elsif ($line =~ m/XLINE/) {
 		$line =~ s/^\*\*\* XLINE: //;
-		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20X:Line\cC30]\cC20:\cC30", $line);
+		HexChat::emit_print('Generic Message', "\cB\cC30[\cC20X:Line\cC30]", $line);
 		return 1;
 	}
 	return 0;
